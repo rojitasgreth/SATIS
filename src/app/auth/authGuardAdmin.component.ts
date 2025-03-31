b3451b51ffaf2eb13f0da7a44811d7d9432661bb
grojas <grojas@mppp.gob.ve>
2025-03-31 09:55:53 -0400

    update

--- src/app/auth/authGuardAdmin.component.ts
+++ src/app/auth/authGuardAdmin.component.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardAdmin implements CanActivate {
+export class AuthGuardAdmin  {
   autenticado: boolean = false;
   constructor(
     private router: Router,
