556b99716a082e5d611901d62dcf1efceeaf2dc6
grojas <grojas@mppp.gob.ve>
2025-04-01 11:51:25 -0400

    b

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     BASE_URL_API: 'https://www.satisccs.site/satis',
-    BASE_URL: 'https://www.satiscss.site/'
+    BASE_URL: 'https://www.satisccs.site/'
 };
