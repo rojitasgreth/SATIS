58e0a6ce99fc06eec2c88909275fd4cc7ffcfd08
grojas <grojas@mppp.gob.ve>
2025-06-03 11:11:07 -0400

    agregar, editar y eliminar colores

--- src/app/pages/admin/modals/products/colorProduct.modal.ts
+++ src/app/pages/admin/modals/products/colorProduct.modal.ts
@@ -18,7 +18,11 @@ import Swal from "sweetalert2";
 
 export class colorProductModalComponent {
   coloresForm: FormGroup;
+  editarColores: FormGroup;
   productoData: any;
+  nuevoColor: boolean = false;
+  listaColores: any = [];
+  generoSeleccionado = 'Hembra';
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
     public matDialogRef: MatDialogRef<colorProductModalComponent>,
     private _formBuilder: FormBuilder,
@@ -27,13 +31,145 @@ export class colorProductModalComponent {
   ) {
     const regex = /^[0-9]+$/;
     this.productoData = data;
-    //console.log(this.productoData);
 
     this.coloresForm = this._formBuilder.group({
       colores: this._formBuilder.array([])
     });
 
+    this.editarColores = this._formBuilder.group({
+        editar: this._formBuilder.array([])
+    });
+    this.listarColores('H');
+
     this.createColor();
+  }
+
+  listarColores(genero: string){
+
+    if (genero == 'V') {
+      this.generoSeleccionado = 'Varón';
+    } else {
+      this.generoSeleccionado = 'Hembra';
+    }
+    const data = {
+      "cod_producto": this.productoData.cod_producto,
+      "genero": genero
+    };
+
+    this.http.post(`${environment.BASE_URL_API}/listarColores`, data).subscribe(
+      (response: any) => {
+        if (response !== 'VACIO') {
+          this.listaColores = response;
+
+          const coloresArray = this.listaColores.map((item: any) => {
+            return this._formBuilder.group({
+              codigo_color: [item.codigo_color, Validators.required],
+              descripcion_color: [item.descripcion_color, Validators.required],
+              cod_producto: [this.productoData.cod_producto, Validators.required],
+              genero: [genero, Validators.required]
+            });
+          });
+
+          this.editarColores.setControl('editar', this._formBuilder.array(coloresArray));
+          this.cdr.detectChanges();
+        } else {
+          this.listaColores = [];
+          this.editarColores.setControl('editar', this._formBuilder.array([]));
+        }
+      },
+      (error: any) => {
+        console.error("Error", error);
+      }
+    );
+  }
+
+  guardarColor(index: any){
+    // Obtener el grupo de formulario específico
+  const colorFormGroup = this.editar.controls[index] as FormGroup;
+
+  // Verificar si el formulario es válido
+  if (colorFormGroup.invalid) {
+    // Marcar todos los campos como tocados para mostrar errores
+    colorFormGroup.markAllAsTouched();
+    return;
+  }
+
+  // Preparar los datos para enviar al backend
+  const datosParaEnviar = {
+    codigo_color: colorFormGroup.value.codigo_color,
+    descripcion_color: colorFormGroup.value.descripcion_color, // Este es el campo editado
+    cod_producto: colorFormGroup.value.cod_producto,
+    genero: colorFormGroup.value.genero
+  };
+
+  this.http.post(`${environment.BASE_URL_API}/modificarColores`, datosParaEnviar).subscribe({
+    next: (response) => {
+      // Mostrar mensaje de éxito
+      this.showSuccessMessage('Color actualizado correctamente');
+
+      // Opcional: Actualizar la lista de colores
+      this.listarColores(datosParaEnviar.genero);
+    },
+    error: (err) => {
+      console.error('Error al guardar:', err);
+      this.showWrongMessage;
+    }
+  });
+
+  }
+
+  eliminarColor(index: number){
+    Swal.fire({
+      title: "¿Está seguro que desea eliminar el color seleccionado?",
+      text: `Una vez eliminado no podrá recuperarse`,
+      confirmButtonText: 'Si, eliminar',
+      confirmButtonColor: '#0097A7',
+      cancelButtonText: 'Cancelar',
+      cancelButtonColor: '#D32F2F',
+      showCancelButton: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        const colorFormGroup = this.editar.controls[index] as FormGroup;
+
+        // Verificar si el formulario es válido
+        if (colorFormGroup.invalid) {
+          // Marcar todos los campos como tocados para mostrar errores
+          colorFormGroup.markAllAsTouched();
+          return;
+        }
+
+        // Preparar los datos para enviar al backend
+        const datosParaEnviar = {
+          codigo_color: colorFormGroup.value.codigo_color,
+          descripcion_color: colorFormGroup.value.descripcion_color, // Este es el campo editado
+          cod_producto: colorFormGroup.value.cod_producto,
+          genero: colorFormGroup.value.genero
+        };
+
+        this.http.post(`${environment.BASE_URL_API}/eliminarColor`, datosParaEnviar).subscribe({
+          next: (response) => {
+            this.showSuccessMessage('Color eliminado correctamente');
+            this.listarColores(datosParaEnviar.genero);
+          },
+          error: (err) => {
+            console.error('Error al guardar:', err);
+            this.showWrongMessage;
+          }
+        });
+
+      }
+    })
+
+  }
+
+  agregarForm(){
+    this.nuevoColor = !this.nuevoColor;
+    this.cdr.detectChanges();
+  }
+
+  get editar(): FormArray {
+    return this.editarColores.get('editar') as FormArray;
   }
 
   get colores() {
@@ -69,13 +205,13 @@ export class colorProductModalComponent {
       this.http.post(`${environment.BASE_URL_API}/insertarColores`, form).subscribe(
         (response) => {
           if (response == 'Insercion correcta') {
-            this.showSuccessMessage();
+            this.showSuccessMessage('Color guardado exitosamente');
             this.cerrar('exitoso');
           } else {
             this.showWrongMessage();
           }
 
-          
+
         },
         (error) => {
           Swal.fire({
@@ -102,10 +238,10 @@ export class colorProductModalComponent {
     });
   }
 
-  showSuccessMessage() {
+  showSuccessMessage(mensaje: string) {
     Swal.fire({
       icon: 'success',
-      title: 'Color cargado exitosamente',
+      title: mensaje,
       showConfirmButton: true,
       confirmButtonAriaLabel: 'De acuerdo',
       confirmButtonColor: '#0097A7',
