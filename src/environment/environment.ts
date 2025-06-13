b0d84dfeedd38479f60842a3cdf5bf9386f21ae6
Grethmar Rojas <grojas@mppp.gob.ve>
2025-06-12 21:48:41 -0400

    env con base de la api plano

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     BASE_URL_API: 'https://www.satisccs.site/satis',
+    BASE_URL_API_PLAIN: 'https://www.satisccs.site',
     BASE_URL: 'https://www.satisccs.site/'
 };
